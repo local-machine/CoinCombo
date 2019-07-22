3d52f8d1fbb15b90f69d36ff3192d51cf00b51ce
Student <example@example.com>
2019-07-22 15:18:18 -0700

    added test for target value zero

--- Coin.Tests/ModelTests/CoinTests.cs
+++ Coin.Tests/ModelTests/CoinTests.cs
@@ -5,16 +5,22 @@ using Coins.Model;
 namespace Coins.Test
 {
     [TestClass]
-    public class ProgramTest
+    public class CoinTest
     {
         [TestMethod]
         public void CoinConstructor_CreatesInstanceOfCoin_Coin()
         {
             Coin myCoin = new Coin();
-            String str = new String("hakjsdf");
-            Assert.AreEqual(typeof(String), myCoin.GetType());
-            
+            Assert.AreEqual(typeof(Coin), myCoin.GetType());
+        }
 
+        [TestMethod]
+        public void ValueIsZero_MinCoin()
+        {
+            int zero = 0;
+            Coin testCoin = new Coin();
+            int result = testCoin.MinCoins(zero);
+            Assert.AreEqual(zero, result);
         }
     }
 }
