8766c2e8fd088fe8d86b465726486dfc23b3089c
Jessica Munoz and Rochelle Roberts <jessica@>
2019-07-23 11:56:51 -0700

    added min coin method and tests

--- Coin.Tests/ModelTests/CoinTests.cs
+++ Coin.Tests/ModelTests/CoinTests.cs
@@ -4,23 +4,37 @@ using Coins.Model;
 
 namespace Coins.Test
 {
-    [TestClass]
-    public class CoinTest
+  [TestClass]
+  public class CoinTest
+  {
+    [TestMethod]
+    public void CoinConstructor_CreatesInstanceOfCoin_Coin()
     {
-        [TestMethod]
-        public void CoinConstructor_CreatesInstanceOfCoin_Coin()
-        {
-            Coin myCoin = new Coin();
-            Assert.AreEqual(typeof(Coin), myCoin.GetType());
-        }
-
-        [TestMethod]
-        public void ValueIsZero_MinCoin()
-        {
-            int zero = 0;
-            Coin testCoin = new Coin();
-            int result = testCoin.MinCoins(zero);
-            Assert.AreEqual(zero, result);
-        }
+      Coin myCoin = new Coin();
+      Assert.AreEqual(typeof(Coin), myCoin.GetType());
     }
+
+    [TestMethod]
+    public void ValueIsZero_MinCoin()
+    {
+      int zero = 0;
+      Coin testCoin = new Coin();
+      int result = testCoin.MinCoins(zero);
+      Assert.AreEqual(zero, result);
+    }
+    [TestMethod]
+    public void MinCoins_ResultofOne()
+    {
+      Coin myCoin = new Coin();
+      int testResult = myCoin.MinCoins(25);
+      Assert.AreEqual(1, testResult);
+    }
+    [TestMethod]
+    public void MinCoins_ResultofFour()
+    {
+      Coin myCoin = new Coin();
+      int secondResult = myCoin.MinCoins(100);
+      Assert.AreEqual(4, secondResult);
+    }
+  }
 }
